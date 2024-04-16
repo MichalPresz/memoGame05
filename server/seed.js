8a5b949fa1f8724e97d50be5df4bd816fdada987
Nik <Tilija_nik@hotmail.co.uk>
2024-04-16 11:34:28 +0100

    made server

---
+++ server/seed.js
@@ -0,0 +1,9 @@
+import Database from "better-sqlite3";
+const db = new Database("database.db");
+
+db.exec(`CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    time INTEGER
+)
+`);
