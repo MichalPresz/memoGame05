8812c38bf73408e9f5337dccb70f1cfe8407c47f
Nik <Tilija_nik@hotmail.co.uk>
2024-04-17 09:34:12 +0100

    working

--- server/seed.js
+++ server/seed.js
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 const db = new Database("database.db");
 
-db.exec(`CREATE TABLE IF NOT EXISTS messages(
+db.exec(`CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
     username TEXT,
     turns INTEGER
