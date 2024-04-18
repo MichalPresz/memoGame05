0f0eea673d638d49ef6d855143a26c32a3098828
Nik <Tilija_nik@hotmail.co.uk>
2024-04-18 15:06:26 +0100

    fixed the program

--- client/app.js
+++ client/app.js
@@ -205,14 +205,6 @@ generateCards();
 async function handleSubmit(event) {
   event.preventDefault();
   const username = event.target.username.value;
-
-  const errorMessage = document.getElementById("error-message");
-  if (username === "") {
-    errorMessage.textContent = "please enter a username";
-    return;
-  }
-  errorMessage.textContent = "";
-
   console.log(`username:${username}, turns:${turns}`);
   await fetch("https://memogame05.onrender.com/", {
     method: "POST",
